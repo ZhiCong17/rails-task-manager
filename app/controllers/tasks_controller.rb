9b1f954d393e207b805e0f5fc45bfa74da8f7fd7
ZhiCong Ore <zhicong1989@gmail.com>
2024-05-23 14:52:49 +0800

    Added new task function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,9 +4,21 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    redirect_to task_path(@task) if @task.save
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
