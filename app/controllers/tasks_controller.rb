19db51d554eefd07b8f294e14d16905bbf6cd373
ZhiCong Ore <zhicong1989@gmail.com>
2024-05-23 15:16:08 +0800

    Added edit task function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,6 +13,18 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
+
+    redirect_to task_path(@task) if @task.save
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
     redirect_to task_path(@task) if @task.save
   end
 
